a38a96e250ca8b88095fee89ac77c7131382eed6
Henrique M. Seffrin <yugi.toon@gmail.com>
2014-10-30 17:46:13 -0200

    Fixes bug in audio redirection

--- src/br/com/pat2math/action/AudioController.java
+++ src/br/com/pat2math/action/AudioController.java
@@ -1,15 +1,23 @@
-package br.com.pat2math.action;
+/*package br.com.pat2math.action;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
-@RequestMapping("/audio")
+import br.com.pat2math.studentModel.Student;
+
+@Controller @Transactional
 public class AudioController {
 
-	@RequestMapping("")
-	public String terms() {
+	@RequestMapping(method = RequestMethod.GET, value= "/audio")
+	public String audio(Model model, HttpSession session) {
+		Student student = new CurrentUser(session).student();
+		model.addAttribute("student", student);
 		return "audio";
 	}
 	
-}
+}*/
