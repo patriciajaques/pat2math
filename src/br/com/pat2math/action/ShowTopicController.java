41dfdc78ab8bbc2546b92e9f21bb27746347bb2a
cghilardi <cassioghilardi@hotmail.com>
2014-11-27 14:09:47 -0200

    tries to load topics faster

--- src/br/com/pat2math/action/ShowTopicController.java
+++ src/br/com/pat2math/action/ShowTopicController.java
@@ -1,13 +1,19 @@
 package br.com.pat2math.action;
 
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import br.com.pat2math.domainBase.Task;
 import br.com.pat2math.service.SetOfTasksService;
 import br.com.pat2math.studentModel.Student;
@@ -20,10 +26,16 @@ public class ShowTopicController {
 	@Autowired private SetOfTasksService setService;
 	
 	@RequestMapping(value="showTopic", method = RequestMethod.GET)
-	public String loadTasks(Long idSet, Model model, HttpSession session) {
+	public String loadTasks(Long idSet, Model model, HttpSession session, HttpServletRequest request, HttpServletResponse response) {
 		Student student = new CurrentUser(session).student();
 		if(student == null) return "login";
 		List<Task> tasks = setService.showTopic(idSet, student);
+		
+		// TODO: remove this check as soon as possible
+		// Http 1.0 request ?
+		if(request.getProtocol().contains("1.0")) {
+			response.setHeader("Connection", "close");
+		}
 		model.addAttribute("tasks", tasks);
 		return "set.tasks";
 	}
