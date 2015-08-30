ef9c97cd78ec0849bc969a914aa93ec0c9863fcf
Otavio Azevedo <otavioazevedo@outlook.com.br>
2015-08-30 00:43:43 -0300

    Ajuste tecnico

--- WebContent/js/pat2math.js
+++ WebContent/js/pat2math.js
@@ -600,20 +600,6 @@ function nextEquationClick ( ) {
 	
 }
 
-function testandos ( ) {
-	$().guider({
-		title: "Parabéns! Você passou de fase!",
-		description: "O próximo plano de aula contém equações um pouco mais desafiadoras do que o anterior. Lembre-se que você pode pedir uma dica a qualquer momento na resolução de uma equação.",
-		alignButtons: "center",
-		buttons: {
-			Próximo: {
-				click: function() {$.guider({}).hideAll(); loadTasks(numUnlockedPlans);},
-				className: "primary"
-			}
-		}
-	}).show();
-}
-
 //Array original das equações
 var ids = [13, 14, 15, 16, 107, 108, 109, 110, 21, 22, 23, 24, 25, 26,
            29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
