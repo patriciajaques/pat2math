9f282b8b68d30f659f0633571f8115e4741cf480
Otavio Azevedo <otavioazevedo@outlook.com.br>
2015-10-30 20:54:23 -0200

    Ajuste tecnico

--- WebContent/patequation/js/index.js
+++ WebContent/patequation/js/index.js
@@ -886,7 +886,7 @@ $(document).ready(function() {
 			document.getElementById('hintText').style.left = "40%";
 	}
 
-	setTimeout (function(){if (selectedEquation.equation === "x=1") {$("#topics").fadeIn(); $("#topicsAux").hide();}}, 1000);
+	setTimeout (function(){if (selectedEquation.equation === "x=1") {$("#topics").fadeIn(); $("#topicsAux").hide();}}, 4000);
 
 	
     // $("#hintText").hide();
